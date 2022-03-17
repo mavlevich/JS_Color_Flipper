c44be903b797cff81feedda6e8bbd7ca05c6f2e1
Eugeniusz Mowlewicz <by.eugeniusz@gmail.com>
2022-03-17 21:33:18 +0100

    commit

--- app.js
+++ app.js
@@ -1,2 +1,19 @@
-const color = ["green", "red", "rgba(133,122,200)",
+const colors = ["green", "red", "rgba(133,122,200)",
     "#f15025"];
+const  btn = document.getElementById('btn');
+const  color = document.querySelector(".color");
+
+btn.addEventListener("click", function (){
+    //get random number between 0 - 3
+    //Мы генерируем число из определённого диапазона и цвет под этим номером будет высвечиваться
+    const randomNumber = getRandomNumber();
+    console.log(randomNumber);
+
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+
+});
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * colors.length);
+}
